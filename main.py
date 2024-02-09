cdbd845ece932e73af12ac3236bdedb3934d7f29
faizi7867 <34569664+faizi7867@users.noreply.github.com>
2024-02-09 23:00:15 +0530

    Update main.py
    adding multi threading using threads

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-
+import threading
 from Program1 import *
 from Program2 import Square
 
 def main():
     text = './files/profram1_file.txt'
-    frequencies = compute_word_frequency_from_file(text)
-    for i in frequencies.items():
-        print(i)
-    # print(frequencies)
-
+    thread1 = threading.Thread(target=compute_word_frequency_from_file, args=(text,))
     square = Square(4)
-    print(square.area())
+    thread2 = threading.Thread(target=square.area)
+
+    thread1.start()
+    thread2.start()
+
+    # Wait for both threads to finish
+    thread1.join()
+    thread2.join()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
